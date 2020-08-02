fa08402993532f2d3b77b29591b87d963949eb00
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-08-02 16:48:09 -0500

    Created the action selsctor and move selector the move selector will highlight the move but will not actually  chose the move just yet.

---
+++ Pokemon/Assets/Scripts/Battle/BattleDialogBox.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogBox : MonoBehaviour
+{
+    [SerializeField] float textSpeed;
+    [SerializeField] Color highLightedColor;
+
+    [SerializeField] Text dialogText;
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;
+    }
+
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / textSpeed);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction) //sets the highlighted color for choosin an action
+    {
+        for (int i=0; i<actionTexts.Count; ++i)
+        {
+            if(i == selectedAction)
+            {
+                actionTexts[i].color = highLightedColor;
+            }
+            else
+            {
+                actionTexts[i].color = Color.black;//sets color of non selected action to black. 
+            }
+        }
+
+        
+    }
+
+    public void UpdateMoveSelection(int selectedMove, Move move)
+    {
+        for (int i = 0; i < moveTexts.Count; ++i)
+        {
+            if (i == selectedMove)
+            {
+                moveTexts[i].color = highLightedColor;
+            }
+            else
+            {
+                moveTexts[i].color = Color.black;
+            }
+        }
+
+        ppText.text = $"PP {move.PP}/{move.Base.PP}";
+        typeText.text = move.Base.Type.ToString();
+    }
+    
+
+    public void SetMoveNames(List<Move> moves)
+    {
+        for (int i=0; i<moveTexts.Count; ++i)
+        {
+            if(i < moves.Count)
+            {
+                moveTexts[i].text = moves[i].Base.Name;
+            }
+            else
+            {
+                moveTexts[i].text = "-";
+            }
+        }
+    }
+}
