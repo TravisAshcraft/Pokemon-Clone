3f64bd627e19d2ee9660c84a533e6701941e634d
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-08-03 23:22:26 -0500

    Added more battle shit.
    The player can now successfully choose which move it wants to use and the game will execute the move and calculate the damage being dealt. The health bar is now animated to imitate the health being lost. Still need to add specific Type advantages to move calculations.

--- Pokemon/Assets/Scripts/Battle/BattleHUD.cs
+++ Pokemon/Assets/Scripts/Battle/BattleHUD.cs
@@ -8,11 +8,20 @@ public class BattleHUD : MonoBehaviour
     [SerializeField] Text nameText;
     [SerializeField] Text levelText;
     [SerializeField] HPBar hpBar;
-    
+
+    Pokemon _pokemon;
+
     public void SetData(Pokemon pokemon)
     {
+        _pokemon = pokemon;
+
         nameText.text = pokemon.Base.Name;
         levelText.text = "Lvl " + pokemon.Level;
         hpBar.SetHp((float)pokemon.HP / pokemon.MaxHP);
+    }
+
+    public IEnumerator UpdateHp()
+    {
+        yield return hpBar.SetHpSmooth((float)_pokemon.HP / _pokemon.MaxHP);
     }
 }
